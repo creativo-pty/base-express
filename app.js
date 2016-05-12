6b0540695c5accf8b26d974570c10a1a2cd208be
Christopher Duncan <cj.duncan.a@gmail.com>
2016-05-12 15:57:08 -0500

    Provide styling to an object

--- app.js
+++ app.js
@@ -10,7 +10,9 @@ nunjucks.configure(__dirname + '/views', {
 
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 app.use(require('./controllers'));
 
 app.listen(port, function() {

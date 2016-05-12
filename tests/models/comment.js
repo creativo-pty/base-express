eb29429d690c4969ce21f256745d2bea0b48c52a
Christopher Duncan <cj.duncan.a@gmail.com>
2016-05-12 16:30:09 -0500

    :shirt: Fix the formmating in test files

--- tests/models/comment.js
+++ tests/models/comment.js
@@ -1,16 +1,15 @@
-should = require('should')
+should = require('should');
 
-Comment = require('../../models/comment')
+Comment = require( __dirname + '/models/comment');
 
 describe('Model Comment', function() {
 
-  it('get', function(done) {
-    var id = '5678'
+    it('get', function(done) {
+        var id = '5678';
+        Comment.get(id, function(err, comment) {
+            comment.id.should.eql(id);
+            done();
+        });
+    });
 
-    Comment.get(id, function(err, comment) {
-      comment.id.should.eql(id)
-      done()
-    })
-  })
-
-})
+});
